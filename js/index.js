d760f378c630967dbb14497f1106caff35d3c6e8
0803QwQ <64982342+0803QwQ@users.noreply.github.com>
2022-04-21 17:04:01 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,9 @@ function setdata(where, id) {
 function uploadImage(where, obj) {
     $().useOCR(where, obj);
 }
+function uploadData(obj) {
+    $().importData(obj);
+}
 function openDemo() {
     var imgLayer = document.getElementById("imgLayer");
     var imgBoxl = document.getElementById("imgBoxl");
@@ -71,14 +74,42 @@ $(function() {
     $.fn.showOCR = function() {
         if (power['ocr']['enable'] == true) {
             $("#ocr-title").css('display', 'block');
+            $("#ocr-inf").css('display', 'block');
             for (var itemkey in items) {
-                $("#" + items[itemkey] + "-ocr").html("<div style='background-color:rgba(255, 255, 255, 0.8);width:140px;font-size:15px;display:block'><span id='" + items[itemkey] + "-ocr-text'>图片识别</span> <input type='file' name='upload' id='" + items[itemkey] + "-upload' onchange=\"uploadImage('" + items[itemkey] + "', this)\" style='display:inline;width:70px' accept='.gif,.png,.jpg,.jpeg,.tiff,.bmp'></div>");
+                $("#" + items[itemkey] + "-ocr").html("<div style='background-color:rgba(255, 255, 255, 0.8);width:140px;font-size:15px;display:block'><span id='" + items[itemkey] + "-ocr-text'>图片识别</span> <input type='file' name='upload' id='" + items[itemkey] + "-upload' onchange=\"uploadImage('" + items[itemkey] + "', this);\" style='display:inline;width:70px' accept='.gif,.png,.jpg,.jpeg,.tiff,.bmp'></div>");
             }
         } else {
             for (var itemkey in items) {
                 $("#" + items[itemkey] + "-ocr").html("<br>");
             }
         }
+    }
+    $.fn.importData = function(obj) {
+        var ext = $('#import-file').val().split('.').pop().toLowerCase();
+        if (ext != 'json') {
+            alert('请上传JSON表单文件！您上传的文件类型为' + ext);
+            return;
+        }
+        var files = obj.files;
+        if (files[0].size > 1048576) {
+            alert('请上传小于1MB的文件！您上传的文件大小为' + (files[0].size / 1048576).toFixed(2) + 'MB');
+            return;
+        }
+        var reader = new FileReader();
+        reader.readAsText(files[0], "UTF-8");
+        reader.onload = function(evt) {
+            json = JSON.parse(evt.target.result);
+            $('#chr-list').val(json['chr']);
+            for (var itemkey in items) {
+                for (var idkey in ids) {
+                    $('#' + items[itemkey] + '-' + ids[idkey]).val(json['info'][items[itemkey]][ids[idkey]]['entry']);
+                    $.fn.setEntry(items[itemkey], ids[idkey], json['info'][items[itemkey]][ids[idkey]]['data']);
+                }
+            }
+            $('#import').html('成功');
+            return;
+        }
+        $('#import').html('失败');
     }
     $.fn.useOCR = async function(where, obj) {
         $('#' + where + '-ocr-text').html('识别中…');
@@ -95,9 +126,7 @@ $(function() {
             return;
         }
         var formData = new FormData();
-        for (var i = 0; i < files.length; i ++) {
-            formData.append("image[]", files[i]);
-        }
+        formData.append("image[]", files[0]);
         await $.ajax({
             type: "post",
             url: "ocr.php",
@@ -211,5 +240,26 @@ $(function() {
             } else {
                 $('#final-level').html("<p style='color:#000000'>NaN</p>");
             }
+    });
+    $("button[name=export]").on('click', function() {
+        exportData = {};
+        exportData['chr'] = $('#chr-list option:selected').val();
+        exportData['info'] = {};
+        for (var itemkey in items) {
+            exportData['info'][items[itemkey]] = {};
+            for (var idkey in ids) {
+                exportData['info'][items[itemkey]][ids[idkey]] = {};
+                exportData['info'][items[itemkey]][ids[idkey]]['entry'] = $('#' + items[itemkey] + '-' + ids[idkey] + ' option:selected').val();
+                exportData['info'][items[itemkey]][ids[idkey]]['data'] = $('#' + items[itemkey] + '-' + ids[idkey] + 'n').val();
+            }
+        }
+        exportDataStr = JSON.stringify(exportData);
+        var blob = new Blob([exportDataStr]);
+        $('#export-link').attr('href', URL.createObjectURL(blob));
+        $('#export-link').attr('download', Date() + " gitoolData.json");
+        $('#export-text').trigger("click");
+    });
+    $("button[name=import]").on('click', function() {
+        $('#import-file').trigger("click");
     });
 });
